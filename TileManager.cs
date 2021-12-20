ea77b9406b4d09efdc758a4d56c532e7163f15bc
VaidyaAnuja <anujavaidya2000@gmail.com>
2021-12-20 19:45:57 +0530

    Twelveth

--- TileManager.cs
+++ TileManager.cs
@@ -7,13 +7,18 @@ public class TileManager : MonoBehaviour
     // Start is called before the first frame update
     public GameObject[] tilePrefabs;
     public float xSpawn = 0;    
-    public float tileLength = 37;
+    public float tileLength = 23;
+    public int numberoftiles = 1;
     void Start()
     {
          //int rand = Random.Range(0,tilePrefabs.Length);
-        SpawnTile(0);
+        //SpawnTile(0);
         //SpawnTile(1);
         //SpawnTile(4);
+
+        for(int i=0; i < numberoftiles ; i++){
+            SpawnTile(0);
+        }
     }
 
     // Update is called once per frame
@@ -23,7 +28,7 @@ public class TileManager : MonoBehaviour
     }
 
     public void SpawnTile(int tileindex){
-        Instantiate(tilePrefabs[tileindex], transform.position * xSpawn, transform.rotation);
+        Instantiate(tilePrefabs[tileindex], new Vector2( xSpawn,0), transform.rotation);
         //Instantiate(objects[rand], transform.position, Quaternion.identity);
         xSpawn += tileLength;
     }
