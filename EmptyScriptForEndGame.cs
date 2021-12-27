9ff335539a5437bcfb16ed8b0e2770da6ac5a9cd
VaidyaAnuja <anujavaidya2000@gmail.com>
2021-12-27 19:56:46 +0530

    TwentyTwo

--- EmptyScriptForEndGame.cs
+++ EmptyScriptForEndGame.cs
@@ -11,9 +11,13 @@ public class EmptyScriptForEndGame : MonoBehaviour
     void Start()
     {
         ScoreTxt.text = "Score: " + displayhealth.score.ToString();
-        
+        if(displayhealth.score > PlayerPrefs.GetInt("HighScore", 0)){
+        PlayerPrefs.SetInt("HighScore", displayhealth.score);
+        }
+        HighScoreTxt.text = "High Score: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
     }
-
+    
+    
     // Update is called once per frame
     void Update()
     {
